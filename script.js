31896547a0e76e073b646670cc8cf9cafc42cdaa
Thomas <ThommyDude@users.noreply.github.com>
2017-12-14 14:46:23 +0100

    Completed
    I included 0 CSS because I'm sick and can't be bothered right now. I might come back to it at some point...

---
+++ script.js
@@ -0,0 +1,91 @@
+$(document).ready(function()
+{
+    var country;
+    var city;
+    
+    fetch("./land.json")
+    .then(function(response)
+    {
+        return response.json();
+    })
+    .then(function(countryCol)
+    {
+        country = countryCol;
+        country.sort(function(a, b) { return a.countryname > b.countryname });
+        createCountryList();
+    });
+
+    function createCountryList()
+    {
+        var $list = $("<div id='countryHolder'></div>")
+            .on({
+                click: function()
+                {
+                    if (this.nextSibling != null)
+                    {
+                        console.log("Sibling");
+                        if (this.nextSibling.getAttribute("class") != "cityList")
+                        {
+                            console.log("no cityList");
+                            getCities(this.id);
+                        }
+                        else
+                        {
+                            console.log("cityList found, removing!");
+                            this.nextSibling.remove();
+                        }
+                    }
+                    else
+                    {
+                        console.log("no sibling");
+                        getCities(this.id);
+                    }
+                }
+            }, ".country");
+
+
+        for(var i = 0; i < country.length; i++)
+        {
+            var $country = $("<div class='country' id=" + country[i].id + ">" + country[i].countryname + "</div>");
+            $list.append($country);
+        }
+        $("body").append($list);
+    }
+
+    function getCities(countryId)
+    {
+        fetch("./stad.json")
+        .then(function(response)
+        {
+            return response.json();
+        })
+        .then(function(cityCol)
+        {
+            city = cityCol;
+            city.sort(function(a, b) { return a.population - b.population });
+            city.reverse();
+            createCityList(countryId);
+        });
+    }
+
+    function createCityList(countryId)
+    {
+        var correctCitys = [];
+        for(var i = 0; i < city.length; i++)
+        {
+            if (city[i].countryid == countryId)
+            {
+                correctCitys.push(city[i]);
+            }
+        }
+
+        var $cityList = $("<ul class='cityList'></ul>");
+        for(var i = 0; i < correctCitys.length; i++)
+        {
+            var $city = $("<li>Name: " + correctCitys[i].stadname + "<br/>Population: " + correctCitys[i].population + "</li>");
+            $cityList.append($city);
+        }
+
+        $cityList.insertAfter("#" + countryId);
+    }
+});

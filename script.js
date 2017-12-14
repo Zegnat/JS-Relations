8401897316facc5d927bd260e48c437ebb84ade0
Martijn van der Ven <martijn@vanderven.se>
2017-12-14 17:22:22 +0100

    Chain from sort
    The sort method always changes the array in place, so I completely
    forgot it also returns the newly sorted array. Because it returns the
    array we can chain on from there.
    
    Cuts out just a little bit of extra JS.

--- script.js
+++ script.js
@@ -2,17 +2,14 @@ Promise.all([
   window.fetch('land.json').then(function (response) { return response.json() }),
   window.fetch('stad.json').then(function (response) { return response.json() })
 ]).then(function (data) {
-  let [countries, allCities] = data
-  countries.sort((a, b) => a.countryname.localeCompare(b.countryname, 'sv'))
-  countries.forEach(country => {
+  let [countries, cities] = data
+  countries.sort((a, b) => a.countryname.localeCompare(b.countryname, 'sv')).forEach(country => {
     let details = document.createElement('details')
     let summary = document.createElement('summary')
     summary.textContent = country.countryname
     details.appendChild(summary)
-    let cities = allCities.filter(city => city.countryid === country.id)
-    cities.sort((a, b) => b.population - a.population)
     let list = document.createElement('ol')
-    cities.forEach(city => {
+    cities.filter(city => city.countryid === country.id).sort((a, b) => b.population - a.population).forEach(city => {
       let item = document.createElement('li')
       item.innerHTML = `Name: ${city.stadname}<br>Population: ${city.population}`
       list.appendChild(item)
